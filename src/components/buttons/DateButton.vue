9b4f8815e62440c2d15224e81677ab2bbe7d7a95
Alex Kist <akist0707@gmail.com>
2023-03-17 14:59:34 -0500

    Create date button for workouts view

---
+++ src/components/buttons/DateButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="DateButton">
+        <BackButton />
+        <button></button>
+        <ForwardButton />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BackButton from '@/components/buttons/BackButton.vue'
+import ForwardButton from '@/components/buttons/ForwardButton.vue'
+
+export default defineComponent({
+  components: {
+    BackButton,
+    ForwardButton
+  },
+  created () {
+    for (let i = 0; i < 25; i++) {
+      if (9 % i === 0 && 16 % i === 0 && 25 % i === 0) {
+        console.log(i)
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.DateButton {
+  display: flex;
+}
+</style>
--- src/views/WorkoutView.vue
+++ src/views/WorkoutView.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>My Workouts</h1>
+    <div class="WorkoutView">
+        <h1>My Workouts</h1>
+        <DateButton />
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import DateButton from '@/components/buttons/DateButton.vue'
+
+export default defineComponent({
+  components: {
+    DateButton
+  }
+})
+</script>
+
+<style lang="scss">
+.WorkoutView {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
