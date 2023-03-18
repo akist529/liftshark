4bd004908810049b82f0a176899a96574df13b85
Alex Kist <akist0707@gmail.com>
2023-03-18 18:10:18 -0500

    Allow user to toggle current day using arrows on date button

--- src/components/buttons/DateButton.vue
+++ src/components/buttons/DateButton.vue
@@ -1,27 +1,80 @@
 <template>
     <div class="DateButton">
-        <BackButton />
-        <button></button>
-        <ForwardButton />
+        <BackButton @click="changeDateBack()" />
+        <button>
+          <img :src="assetspath('./ui/calendar_today.webp')" />
+          <span v-if="dateIsToday()">Today</span>
+          <span v-else>{{ months[selectedMonth] }} {{ selectedDate }}</span>
+        </button>
+        <ForwardButton @click="changeDateForward()" />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { fetchImages } from '@/mixins/fetchImages'
 import BackButton from '@/components/buttons/BackButton.vue'
 import ForwardButton from '@/components/buttons/ForwardButton.vue'
 
 export default defineComponent({
+  data () {
+    const date = new Date()
+    const selectedDate = date.getDate()
+    const selectedDay = date.getDay()
+    const selectedMonth = date.getMonth()
+    const selectedYear = date.getFullYear()
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+    return {
+      date,
+      selectedDate,
+      selectedDay,
+      selectedMonth,
+      selectedYear,
+      months
+    }
+  },
+  watch: {
+    selectedDate (date: number) {
+      if (date < 1) {
+        this.selectedMonth = ((this.selectedMonth += 12) - 1) % 12
+        this.selectedDate = this.daysInMonth(this.selectedYear, this.selectedMonth + 1)
+      }
+
+      if (date > this.daysInMonth(this.selectedYear, this.selectedMonth + 1)) {
+        this.selectedMonth = ((this.selectedMonth += 12) + 1) % 12
+        this.selectedDate = 1
+      }
+    }
+  },
   components: {
     BackButton,
     ForwardButton
   },
-  created () {
-    for (let i = 0; i < 25; i++) {
-      if (9 % i === 0 && 16 % i === 0 && 25 % i === 0) {
-        console.log(i)
+  mixins: [fetchImages],
+  methods: {
+    changeDateBack () {
+      this.selectedDate -= 1
+    },
+    changeDateForward () {
+      this.selectedDate += 1
+    },
+    daysInMonth (year: number, month: number) {
+      return new Date(year, month, 0).getDate()
+    },
+    dateIsToday () {
+      const dateSelected = `${this.selectedYear}-${this.selectedMonth}-${this.selectedDate}`
+      const actualDate = `${this.date.getFullYear()}-${this.date.getMonth()}-${this.date.getDate()}`
+
+      if (actualDate === dateSelected) {
+        return true
+      } else {
+        return false
       }
     }
+  },
+  created () {
+    this.date = new Date()
   }
 })
 </script>
