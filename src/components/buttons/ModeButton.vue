097d9322232d45ba77c9711bcbb83071ccee5e6e
Alex Kist <akist0707@gmail.com>
2023-08-03 14:13:27 -0500

    Refactor light/dark mode button with Vuetify and eliminate slider button

--- src/components/buttons/ModeButton.vue
+++ src/components/buttons/ModeButton.vue
@@ -1,15 +1,22 @@
 <template>
-<button class="ModeButton" @click="modeStore.toggleDarkMode">
-	<span
-		id="light-mode"
-		:class="modeStore.darkMode ? 'inactive' : 'active'"
-	></span>
-	<SliderButton />
-	<span
-		id="dark-mode"
-		:class="modeStore.darkMode ? 'active' : 'inactive'"
-	></span>
-</button>
+<v-switch
+	inset
+	color="info"
+	@click="modeStore.toggleDarkMode"
+	class="d-flex justify-start align-center">
+	<template v-slot:prepend>
+		<v-icon
+			icon="mdi-weather-sunny"
+			:color="modeStore.darkMode ? 'white' : 'orange'"
+		></v-icon>
+	</template>
+	<template v-slot:append>
+		<v-icon
+			icon="mdi-moon-waning-crescent"
+			:color="modeStore.darkMode ? 'info' : 'white'"
+		></v-icon>
+	</template>
+</v-switch>
 </template>
 
 <script lang="ts">
@@ -17,80 +24,22 @@
 import { defineComponent } from 'vue';
 // Pinia stores
 import { useModeStore } from '@/stores/modeStore';
-// Local components
-import SliderButton from '@/components/buttons/SliderButton.vue';
 
 export default defineComponent({
 	data () {
-		const modeStore = useModeStore();
-
 		return ({
-			modeStore
+			modeStore: useModeStore()
 		});
-	},
-	components: {
-		SliderButton
 	}
 });
 </script>
 
 <style scoped lang="scss">
-.ModeButton {
-	/* Positioning */
-	display: flex;
-		justify-content: center;
-		align-items: center;
-		gap: 5px;
-
-	/* Visual */
-	background: none;
-	border: none;
-	cursor: pointer;
-
-	.inactive {
-		filter: invert(1) opacity(30%);
-	}
-
-	.active {
-		filter: invert(1) opacity(100%);
-	}
-
-	#light-mode {
-		display: inline-block;
-		content: '';
-		width: 25px;
-		height: 25px;
-		background-image: url('/public/images/icons/light_mode.svg');
-			background-repeat: no-repeat;
-			background-size: contain;
-			background-position: center;
-	}
-
-	#dark-mode {
-		display: inline-block;
-		content: '';
-		width: 25px;
-		height: 25px;
-		background-image: url('/public/images/icons/dark_mode.svg');
-			background-repeat: no-repeat;
-			background-size: contain;
-			background-position: center;
-	}
-}
-
 @media only screen and (min-width: 992px) {
 	.ModeButton {
 		position: absolute;
 		top: 10px;
 		left: 10px;
-
-		.inactive {
-			filter: invert(0) opacity(30%);
-		}
-
-		.active {
-			filter: invert(0) opacity(100%);
-		}
 	}
 }
 </style>
