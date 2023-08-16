d8fa24db7e23675adba47001fcfef7e8ae2c4de4
Alex Kist <akist0707@gmail.com>
2023-08-15 21:08:02 -0500

    Add date picker functionality to measurement log

--- src/components/cards/MeasurementCard.vue
+++ src/components/cards/MeasurementCard.vue
@@ -15,17 +15,26 @@
     <template
         v-slot:title
     >Measurement Log</template>
-    <v-card-actions>
+    <v-card-actions
+        class="d-flex flex-column justify-center align-center"
+    >
+        <VueDatePicker
+            v-model="month"
+            month-picker
+            :teleport="true"
+            teleport-center
+        />
         <v-select
             v-model="muscle"
             label="Muscle"
             :placeholder="items[0]"
             :items="items"
+            class="w-100 pt-3"
         ></v-select>
     </v-card-actions>
     <v-card-text>
         <v-table
-            v-if="filteredMeasurements().length"
+            v-if="measurements.length"
         >
             <tbody>
                 <tr>
@@ -33,7 +42,7 @@
                     <th>MEASUREMENT</th>
                 </tr>
                 <tr
-                    v-for="measurement in filteredMeasurements()"
+                    v-for="measurement in measurements"
                     :key="measurement.id"
                 >
                     <td>{{ measurement.attributes.date }}</td>
@@ -50,26 +59,46 @@
 
 <script lang="ts">
 // Vue imports
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 // Pinia stores
 import { useStatStore } from '@/stores/statStore';
+// Third-party components
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+import { MeasurementData } from '@/types/index';
 
 export default defineComponent({
     data () {
         const statStore = useStatStore();
         const muscle = '';
         const items = ['Upper Arm', 'Forearm', 'Chest', 'Thigh', 'Calf', 'Waist', 'Shoulder'];
+        const month = ref({
+            month: new Date().getMonth(),
+            year: new Date().getFullYear()
+        });
 
         return ({
             statStore,
             muscle,
-            items
+            items,
+            month
         });
     },
-    methods: {
-        filteredMeasurements () {
-            const muscle = this.muscle;
-            return this.statStore.measurements.filter(measurement => measurement.attributes.muscle === muscle);
+    components: {
+        VueDatePicker
+    },
+    computed: {
+        measurements (): MeasurementData[] {
+            return this.statStore.measurements.filter(measurement => {
+                const year = Number(measurement.attributes.date.slice(0, 4));
+                const month = Number(measurement.attributes.date.slice(5, 7)) - 1;
+
+                if (measurement.attributes.muscle === this.muscle && year === this.month.year && month === this.month.month) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
         }
     }
 });
